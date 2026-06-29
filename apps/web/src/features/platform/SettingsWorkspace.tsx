247a2ac207f00e6d1c1a214489b9572b8f237991
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-06-29 15:43:55 -0700

    Change workspace

--- apps/web/src/features/platform/SettingsWorkspace.tsx
+++ apps/web/src/features/platform/SettingsWorkspace.tsx
@@ -1,5 +1,6 @@
 import type { OmnixModuleDefinition } from '../../app/modules';
 import { WorkspacePanel } from '../../design/primitives';
+import { HermesRecentPanel as Recent } from './HermesRecentPanel';
 import { HermesReviewCard } from './HermesReviewCard';
 import { HermesStatusCard } from './HermesStatusCard';
 import { PlatformModuleWorkspace } from './PlatformModuleWorkspace';
@@ -10,6 +11,7 @@ export function SettingsWorkspace({ module }: { module: OmnixModuleDefinition })
       <WorkspacePanel>
         <HermesStatusCard />
         <HermesReviewCard />
+        <Recent />
       </WorkspacePanel>
       <PlatformModuleWorkspace module={module} />
     </>
