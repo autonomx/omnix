dfdcd56284a8431dca48756bc4f5270ea1f270c0
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-07-05 00:52:46 -0700

    SCC-3 route Settings to the Control Center

--- apps/web/src/features/platform/SettingsWorkspace.tsx
+++ apps/web/src/features/platform/SettingsWorkspace.tsx
@@ -1,19 +1,6 @@
 import type { OmnixModuleDefinition } from '../../app/modules';
-import { WorkspacePanel } from '../../design/primitives';
-import { HermesRecentPanel as Recent } from './HermesRecentPanel';
-import { HermesReviewCard } from './HermesReviewCard';
-import { HermesStatusCard } from './HermesStatusCard';
-import { PlatformModuleWorkspace } from './PlatformModuleWorkspace';
+import { SettingsControlCenter } from '../settings/SettingsControlCenter';
 
-export function SettingsWorkspace({ module }: { module: OmnixModuleDefinition }) {
-  return (
-    <>
-      <WorkspacePanel>
-        <HermesStatusCard />
-        <HermesReviewCard />
-        <Recent />
-      </WorkspacePanel>
-      <PlatformModuleWorkspace module={module} />
-    </>
-  );
+export function SettingsWorkspace({ module: _module }: { module: OmnixModuleDefinition }) {
+  return <SettingsControlCenter />;
 }
