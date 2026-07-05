ff79c87381bffdcc99f573b4a0e20c0a4bbbe083
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-07-05 13:12:10 -0700

    SCC-F1.2a define clone form defaults

--- apps/web/src/features/voice-cloning/cloneFormDefaults.ts
+++ apps/web/src/features/voice-cloning/cloneFormDefaults.ts
@@ -1,1 +1,25 @@
-export const cloneDefaultsReady = true;
+export type CloneFormValues = {
+  providerId: string;
+  sampleAssetId: string;
+  profileName: string;
+  referenceText: string;
+  language: string;
+  quality: string;
+};
+
+export type CloneModuleDefaults = {
+  providerId: string;
+  language: string;
+  quality: string;
+};
+
+export function cloneFormDefaults(defaults: CloneModuleDefaults): CloneFormValues {
+  return {
+    providerId: defaults.providerId,
+    sampleAssetId: '',
+    profileName: '',
+    referenceText: '',
+    language: defaults.language,
+    quality: defaults.quality,
+  };
+}
