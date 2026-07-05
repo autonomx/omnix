d8c5bd96b3e8283886f389f7e093b487db411b81
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-07-05 00:30:30 -0700

    SCC-0.3 export settings document helpers

--- apps/web/src/features/settings/index.ts
+++ apps/web/src/features/settings/index.ts
@@ -1,3 +1,6 @@
+export * from './settingsDefaults';
+export * from './settingsDocumentTypes';
+export * from './settingsMerge';
 export * from './settingsOwnership';
 export * from './settingsRegistry';
 export * from './settingsTypes';
