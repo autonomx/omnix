01648e6b112ca3c9ccb6a8fea59a8d3a64e3a4ad
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-07-14 16:46:40 -0700

    SC-0 export companion contracts

---
+++ src/app/desktop_companion/__init__.py
@@ -0,0 +1,27 @@
+"""Desktop companion domain package."""
+
+from .models import (
+    CompanionAttentionDecision,
+    CompanionCommentaryCandidate,
+    CompanionLedgerEntry,
+    CompanionRuntimeStatus,
+    DesktopActivitySignal,
+    DesktopBehaviorState,
+    DesktopCompanionPolicy,
+    DesktopObservation,
+    DesktopObservedChange,
+    DesktopObservedValue,
+)
+
+__all__ = [
+    "CompanionAttentionDecision",
+    "CompanionCommentaryCandidate",
+    "CompanionLedgerEntry",
+    "CompanionRuntimeStatus",
+    "DesktopActivitySignal",
+    "DesktopBehaviorState",
+    "DesktopCompanionPolicy",
+    "DesktopObservation",
+    "DesktopObservedChange",
+    "DesktopObservedValue",
+]
