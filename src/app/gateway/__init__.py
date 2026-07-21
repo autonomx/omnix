7b90a8f01dee4f098be1b9be7d1b0358a23249c1
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-07-20 21:51:18 -0700

    Register Live observation generation routes

--- src/app/gateway/__init__.py
+++ src/app/gateway/__init__.py
@@ -19,6 +19,7 @@ from .live_chat_provider_metrics import install_live_chat_provider_metrics_hook
 from .live_chat_provider_routing import install_live_chat_provider_routing_hook
 from .live_chat_stream_retry import install_live_chat_stream_retry_hook
 from .live_material_context import install_live_material_context_hook
+from .live_observation_generation import install_live_observation_generation_hook
 from .live_voice_cue_asset_routes import install_live_voice_cue_asset_hook
 from .live_voice_diagnostics_routes import install_live_voice_diagnostics_hook
 from .live_voice_runtime_offload import install_live_voice_runtime_offload_hook
@@ -99,6 +100,7 @@ install_live_chat_live_voice_profile_hook()
 install_live_voice_spoken_style_hook()
 install_live_chat_companion_context_hook()
 install_live_material_context_hook()
+install_live_observation_generation_hook()
 install_memory_job_offload_hook()
 install_live_voice_diagnostics_hook()
 install_live_voice_cue_asset_hook()
