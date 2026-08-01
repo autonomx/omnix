c991650021ba7bd0db272d72729a599f94ed8a76
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-08-01 14:18:16 -0700

    Register Character Live2D avatar routes

--- src/app/assistant_context/__init__.py
+++ src/app/assistant_context/__init__.py
@@ -8,6 +8,7 @@ from app.characters.api import register_character_routes
 from app.characters.avatar_api import register_character_avatar_routes
 from app.characters.avatar_generation_api import register_character_avatar_generation_routes
 from app.characters.avatar_viseme_api import register_character_avatar_viseme_routes
+from app.characters.live2d_avatar import register_character_live2d_avatar_routes
 from app.characters.live_conversation_rendering import register_live_conversation_rendering_routes
 from app.desktop_companion.routes import register_desktop_companion_routes
 
@@ -36,6 +37,7 @@ def register_assistant_context_routes(app, **kwargs: Any) -> None:
     register_character_avatar_routes(app)
     register_character_avatar_generation_routes(app)
     register_character_avatar_viseme_routes(app)
+    register_character_live2d_avatar_routes(app)
 
 
 __all__ = [
