c4f90046adde9ba569d712e4c1658fe48d18d0ec
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-08-01 00:13:05 -0700

    Export completed image synchronization helpers

--- apps/web/src/features/image-generation/ImageGenerationWorkspace.tsx
+++ apps/web/src/features/image-generation/ImageGenerationWorkspace.tsx
@@ -1,7 +1,9 @@
 export { ImageGenerationWorkspaceImpl as ImageGenerationWorkspace } from './ImageGenerationWorkspaceMultiModel';
 export {
+  completedImageAssetIds,
   hasActiveImageJobs,
   imageAssetUrl,
+  isCompletedImageJobEventPayload,
   isImageJobEventPayload,
   selectLatestImageAsset,
 } from './imageWorkspaceModel';
