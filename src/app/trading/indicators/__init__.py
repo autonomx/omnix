5d388c5b3c1ffdf3de8d9939df8171d79c74e51a
Autonomx Developer <31607344+autonomxDeveloper@users.noreply.github.com>
2026-08-05 17:20:16 -0700

    OTT-6 create Trading indicator package

---
+++ src/app/trading/indicators/__init__.py
@@ -0,0 +1,15 @@
+"""Versioned deterministic technical indicators."""
+
+from .engine import (
+    CORE_INDICATOR_FORMULA_VERSION,
+    exponential_moving_average,
+    relative_strength_index,
+    simple_moving_average,
+)
+
+__all__ = [
+    "CORE_INDICATOR_FORMULA_VERSION",
+    "simple_moving_average",
+    "exponential_moving_average",
+    "relative_strength_index",
+]
